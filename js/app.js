77a05fdf1845e2228fcf6032714ec60d4771d3b7
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-03-10 22:34:53 +0600

    fifth bug fixed

--- js/app.js
+++ js/app.js
@@ -151,8 +151,11 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  const reportedContainer = document.getElementById( "reported" );
+  reportedContainer.innerHTML = "";
+
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
